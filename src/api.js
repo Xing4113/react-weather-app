2336d57d05fff982d55fb0b2495fd98b249c81d1
Xing4113 <wenxing174@gmail.com>
2023-09-18 21:40:55 +0800

    Done Weather App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const Geo_Options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ecf3d19df6mshc56e35c33a4d6edp1f187cjsn82c72b30e8a7',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1384ce3c32df0725b195ec01780ea602";
---
+++ src/components/search/search.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, Geo_Options } from "../../api";
+
+export const Search = ({ onSearchChange }) => {
+
+    const [search, setSearch] = useState(null);
+
+    const loadOption = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            Geo_Options
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                return {
+                    options: res.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch((err) => console.log(err));
+
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={800}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOption}
+        />
+    )
+}
+
